f023a9059e14c3a788d55554bb0422bb3e3a2bbe
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-22 18:45:26 -0300

    Add files via upload

---
+++ EX5POO/EX5POO/CONVERSOR.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX5POO
+{
+    internal class Conversor
+    {
+        const double MilhaMaritimaParaMetro = 1852; // 1 milha marítima em metros
+        const double MetroParaQuilometro = 0.001;   // 1 metro em quilômetros
+
+        public double ConverterMilhasParaQuilometros(double milhas)
+        {
+            double metros = milhas * MilhaMaritimaParaMetro;
+            double quilometros = metros * MetroParaQuilometro;
+            return quilometros;
+        }
+    }
+}
+
---
+++ EX5POO/EX5POO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX5POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de milhas marítimas: ");
+            double milhas = Convert.ToDouble(Console.ReadLine());
+
+            Conversor conversor = new Conversor();
+            double quilometros = conversor.ConverterMilhasParaQuilometros(milhas);
+
+            Console.WriteLine($"{milhas} milhas marítimas equivalem a {quilometros} quilômetros.");
+        }
+    }
+}
